0452661e4e3ac6e7e8b9094cd239727ceb5aff14
Filipe Brito de Aguiar dos Santos <99624132+devBrito36@users.noreply.github.com>
2023-05-21 21:38:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./Components/Header/header";
+import { GlobalStyle } from "./Components/GlobalStyle";
 
 export default function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
     </>
   );
